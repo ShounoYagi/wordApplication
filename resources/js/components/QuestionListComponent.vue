042c182b439f4fe2146caa7a955775a3a81b629c
yagis <yaginikuppp82@gmail.com>
2021-05-01 10:13:05 +0900

    UI修正

--- resources/js/components/QuestionListComponent.vue
+++ resources/js/components/QuestionListComponent.vue
@@ -24,54 +24,28 @@
             align="right"
             width="100%">
                 <template slot-scope="scope">
-                    <el-button
-                    size="mini"
-                    type="primary"
-                    @click="handleView(scope.row)">表示</el-button>
-                    <el-button
-                    size="mini"
-                    type="success"
-                    @click="handleEdit(scope.row)">編集</el-button>
-                    <el-button
-                    size="mini"
-                    type="danger"
-                    @click="deleteQuestion(scope.row)">削除</el-button>
+                    <el-row>
+                        <el-button
+                        size="mini"
+                        type="primary"
+                        @click="handleView(scope.row)">表示</el-button>
+                    </el-row>
+                    <el-row>
+                        <el-button
+                        size="mini"
+                        type="success"
+                        @click="handleEdit(scope.row)">編集</el-button>
+                    </el-row>
+                    <el-row>
+                        <el-button
+                        size="mini"
+                        type="danger"
+                        @click="deleteQuestion(scope.row)">削除</el-button>
+                    </el-row>
                 </template>
             </el-table-column>
         </el-table>
 
-        <!-- <table class="table table-hover">
-            <thead class="thead-light">
-            <tr align="center">
-                <th scope="col">問題文</th>
-                <th scope="col">答え</th>
-                <th scope="col">カテゴリー１</th>
-                <th scope="col">カテゴリー２</th>
-                <th scope="col">カテゴリー３</th>
-                <th scope="col">メニュー</th>
-            </tr>
-            </thead>
-            <tbody>
-                <tr v-for="question in questions" v-bind:key="question.id" align="center">
-                    <td>{{ question.QuestionText }}</td>
-                    <td>{{ question.Answer }}</td>
-                    <td>{{ question.Category1 }}</td>
-                    <td>{{ question.Category2 }}</td>
-                    <td>{{ question.Category3 }}</td>
-                    <td>
-                        <router-link v-bind:to="{name: 'question.show', params: {questionId: question.id }}">
-                            <button class="btn btn-primary">表示</button>
-                        </router-link>
-                   
-                        <router-link v-bind:to="{name: 'question.edit', params: {questionId: String(question.id) }}">
-                            <button class="btn btn-success">編集</button>
-                        </router-link>
-                  
-                        <button class="btn btn-danger" v-on:click="deleteQuestion(question.id)">削除</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table> -->
     </div>
 </template>
 
--- resources/js/components/QuestionSetsListComponent.vue
+++ resources/js/components/QuestionSetsListComponent.vue
@@ -18,55 +18,27 @@
                 align="right"
                 width="100%">
                     <template slot-scope="scope">
-                        <el-button
-                        size="mini"
-                        type="primary"
-                        @click="handleView(scope.row)">表示</el-button>
-                        <el-button
-                        size="mini"
-                        type="success"
-                        @click="handleEdit(scope.row)">編集</el-button>
-                        <el-button
-                        size="mini"
-                        type="danger"
-                        @click="deleteQuestionSets(scope.row)">削除</el-button>
+                        <el-row>
+                            <el-button
+                            size="mini"
+                            type="primary"
+                            @click="handleView(scope.row)">表示</el-button>
+                        </el-row>
+                        <el-row>
+                            <el-button
+                            size="mini"
+                            type="success"
+                            @click="handleEdit(scope.row)">編集</el-button>
+                        </el-row>
+                        <el-row>
+                            <el-button
+                            size="mini"
+                            type="danger"
+                            @click="deleteQuestionSets(scope.row)">削除</el-button>
+                        </el-row>
                     </template>
                 </el-table-column>
             </el-table>
-        
-
-
-        <!-- <mq-layout mq="other">
-            <table class="table table-hover">
-                <thead class="thead-light">
-                <tr align="center">
-                    <th scope="col">セット名</th>
-                    <th scope="col">問題数</th>
-                    <th scope="col">メニュー</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="questionSet in questionSets" v-bind:key="questionSet.id" align="center">
-
-                        <td>{{ questionSet.Name }}</td>
-
-                        <td>{{ questionSet.QuestionsNum }}</td>
-
-                            <td>
-                                <router-link v-bind:to="{name: 'examination', params: {questionSetId: String(questionSet.id) }}">
-                                    <button class="btn btn-primary">問題を解く</button>
-                                </router-link>
-                    
-                                <router-link v-bind:to="{name: 'questionSets.edit', params: {questionSetId: String(questionSet.id) }}">
-                                    <button class="btn btn-success">編集</button>
-                                </router-link>
-                        
-                                <button class="btn btn-danger" v-on:click="deleteQuestionSets(questionSet.id)">削除</button>
-                            </td>
-                    </tr>
-                </tbody>
-            </table>
-        </mq-layout> -->
 
     </div>
 </template>
